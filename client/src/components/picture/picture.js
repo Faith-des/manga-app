23a7227a62baada9a384de0de7b30e248f944d21
vera <vera@votuv.com>
2020-12-14 19:14:30 +0300

    Add: new logic for client post

--- client/src/components/picture/picture.js
+++ client/src/components/picture/picture.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import './picture.css'
 import img from './img/img-1.jpg'
 
@@ -12,7 +11,7 @@ export default class Picture extends Component {
     y1: 0
   };
 
-  onPictureClick = (e) => {
+  saveCoordinates = (e) => {
     const bounding = e.target.getBoundingClientRect();
     const xCoords = Math.floor(e.clientX - bounding.left);
     const yCoords = Math.floor(e.clientY - bounding.top);
@@ -30,49 +29,33 @@ export default class Picture extends Component {
         y1: yCoords
       });
     }
+  };
 
+  sendCoordinates = () => {
+    const { x, y, x1, y1 } = this.state;
+
+    //fetch('https://httpbin.org/post', {
+    fetch('http://localhost:8081/api/v1/area', {
+      method: 'POST',
+      cors: "no-cors",
+      body: JSON.stringify({ 'dot1': [x, y], 'dot2': [x1, y1], 'path': img })
+    })
+      .then(response => response.json())
+      .then(json_object => console.log(json_object));
   };
 
   render() {
-    const { x, y, x1, y1 } = this.state;
-
-    if (x !== 0 && y !== 0 && x1 !== 0 && y1 !== 0) {
-      //fetch('https://httpbin.org/post', {
-      fetch('http://localhost:8081/api/v1/area', {
-        method: 'POST',
-        cors: "no-cors",
-//        headers: {
-//          'Content-Type': 'application/json',
-//          'Access-Control-Allow-Origin': '*'
-//        },
-        body: JSON.stringify({ 'dot1': [x, y], 'dot2': [x1, y1], 'path': img })
-      })
-        .then(response => response.json())
-        .then(json_object => console.log(json_object));
-    }
 
     return (
       <div className="picture">
         <img className="img"
              src={ img }
              alt="manga page 1"
-             onMouseDown={ this.onPictureClick }
-             onMouseUp={ this.onPictureClick }
+             onMouseDown={ this.saveCoordinates }
+             onMouseUp={ this.saveCoordinates }
+             onClick={ this.sendCoordinates }
         />
       </div>
     )
   }
 }
-
-// onClick={ fetch('https://httpbin.org/post', {
-//   method: 'POST',
-//     cors: "no-cors",
-//     headers: {
-//     'Content-Type': 'application/json',
-//       'Access-Control-Allow-Origin': '*'
-//   },
-//   body: JSON.stringify({ 'X': x, 'Y': y, 'path': img })
-// })
-// .then(response => response.json())
-//   .then(json_object => console.log(json_object))
-// }
